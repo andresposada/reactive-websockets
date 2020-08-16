d34f77d93b76e4c726a6fa21dd34b5e80d87b0b1
Andrés Felipe Posada Ramírez <andres.posada@cadena.com.co>
2020-08-16 18:57:02 -0500

    First commit

---
+++ src/test/java/com/afp/reactivewebsocket/ReactiveWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afp.reactivewebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
